2f301ad61267b4c842f21b8e9b8b36c1f9457bd7
Erl Jocson <jocsonerl@gmail.com>
2022-03-08 16:16:45 +0800

    added blog form

--- BlogProject/post/views.py
+++ BlogProject/post/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from .models import Blog
 
 def index_view(request):
-    return render(request, "index.html", {})
+    if request.method == "POST":
+        try:
+            content = request.POST['content']
+        except:
+            content = ''
+            
+        if content:
+            Blog.objects.create(
+                content=content,
+                user_id=request.user
+            )
+
+    return render(request, "index.html", {
+        "title":"Home",
+    })
