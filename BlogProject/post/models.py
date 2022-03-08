6c5c4dabf206ab82d21f5021f9c62f66db457059
Erl Jocson <jocsonerl@gmail.com>
2022-03-08 14:50:42 +0800

    created models

--- BlogProject/post/models.py
+++ BlogProject/post/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+class Blog(models.Model):
+    content = models.TextField(max_length=256)
+    date = models.DateTimeField(default=now, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+class Comment(models.Model):
+    content = models.TextField(max_length=256)
+    date = models.DateTimeField(default=now, editable=False)
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+class Like(models.Model):
+    is_like = models.BooleanField()
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.is_like
